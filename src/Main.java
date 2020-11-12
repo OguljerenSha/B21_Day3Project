1f82f9b227e4a02e0bcd3e7a382509a3dac02cba
Jeren <jerenshamuhamedova@gmail.com>
2020-11-12 09:38:20 -0600

    Added some more codes to initial

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello git last day");
+        System.out.println("i like my friends");
+        System.out.println("how to be leader");
     }
 }
